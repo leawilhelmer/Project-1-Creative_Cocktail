82275dcde824a67ca4cba858ec126fcde7ca9af8
Victor Tran <victortran415@gmail.com>
2020-11-12 13:53:04 -0800

    add 542 different drinks

--- assets/script.js
+++ assets/script.js
@@ -18,10 +18,12 @@
 
 
 
+
+//NOTE: this will be added to List of Drinks
 var drinksDiv = document.querySelector("#drinksDiv")
 
 function drinkData() {
-	fetch("https://the-cocktail-db.p.rapidapi.com/popular.php", {
+	fetch("https://the-cocktail-db.p.rapidapi.com/filter.php?a=Alcoholic", {
 		"method": "GET",
 		"headers": {
 			"x-rapidapi-key": "1b17814413msh79012f3c019efc8p1c7fb7jsn69899d6e8800",
@@ -32,25 +34,52 @@ function drinkData() {
 		response.json())
 	.then(cocktailData => {
 		console.log(cocktailData)
-		console.log(cocktailData.drinks.length)
+		// console.log(cocktailData.drinks.length)
 		var drinksInfo = cocktailData.drinks
 
 		for (var i = 0 ; i < drinksInfo.length; i++) {
 			var drinksEle = drinksInfo[i];
 			var drinksName = drinksEle.strDrink
 			var drinksImg = drinksEle.strDrinkThumb
-			drinksDiv.innerHTML += `<div class="col s3"><h5 style="text-align: center">${drinksName}</h5><img src=${drinksImg} style="width: 300px; border-radius: 10px;"/></div>`
-
-
+			drinksDiv.innerHTML += `<div class="col s3"><h6 style="text-align: center"><strong>${drinksName}</strong></h6><img src=${drinksImg} style="width: 280px; border-radius: 10px;"/></div>`
 		}
 	})
 	.catch(err => {
 		console.error(err);
 	});
-
 }
-
 drinkData()
+
+
+//NOTE: this will be added to Most Popular Drinks html
+var popularDrink = document.querySelector("#popular-div")
+
+function mostPopularDrinks() {
+	fetch("https://the-cocktail-db.p.rapidapi.com/popular.php", {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "1b17814413msh79012f3c019efc8p1c7fb7jsn69899d6e8800",
+			"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com"
+		}
+	})
+	.then(response =>
+		response.json())
+	.then(moreData => {
+		console.log(moreData)
+		var drinksInfo = moreData.drinks
+
+		for (var i = 0 ; i < drinksInfo.length; i++) {
+			var drinksEle = drinksInfo[i];
+			var popDrinksName = drinksEle.strDrink
+			var popDrinksImg = drinksEle.strDrinkThumb
+			popularDrink.innerHTML += `<div class="col s3"><h6 style="text-align: center"><strong>${popDrinksName}</strong></h6><img src=${popDrinksImg} style="width: 280px; border-radius: 10px;"/></div>`
+		}
+	})
+	.catch(err => {
+		console.error(err);
+	})
+}
+mostPopularDrinks()
 
 
 // NOTE: Adding randomize drinks to index.html
@@ -60,6 +89,7 @@ const flipcardFront = document.getElementById("flipcardFront");
 //This variable is for the div with id 'flipcardBack'
 const flipcardBack = document.getElementById("flipcardBack");
 
+function randomizeDrinks() {
 //Here we are fetching the most popular drinks from the cocktail API
 fetch("https://the-cocktail-db.p.rapidapi.com/popular.php", {
 	method: "GET",
@@ -90,35 +120,37 @@ fetch("https://the-cocktail-db.p.rapidapi.com/popular.php", {
 			data.drinks[random].strIngredient5,
 			data.drinks[random].strIngredient6,
 			data.drinks[random].strIngredient7,
-    ];
-    let measurementsArr = [
-		data.drinks[random].strMeasure1,
-		data.drinks[random].strMeasure2,
-		data.drinks[random].strMeasure3,
-		data.drinks[random].strMeasure4,
-		data.drinks[random].strMeasure5,
-		data.drinks[random].strMeasure6,
-		data.drinks[random].strMeasure7,
-    ];
-    // We intialize 'x' as an empty string
-    let x = "";
-    // We loop through the array
-    for (var i = 0; i <= ingredientsArr.length; i++) {
-		//if the next ingredient is 'null' that means
-		//the rest are null, so we stop the loop
-		//and keep the actual ingredient
-		if (ingredientsArr[i] === null) {
-			break;
+		];
+		let measurementsArr = [
+			data.drinks[random].strMeasure1,
+			data.drinks[random].strMeasure2,
+			data.drinks[random].strMeasure3,
+			data.drinks[random].strMeasure4,
+			data.drinks[random].strMeasure5,
+			data.drinks[random].strMeasure6,
+			data.drinks[random].strMeasure7,
+		];
+		// We intialize 'x' as an empty string
+		let x = "";
+		// We loop through the array
+		for (var i = 0; i <= ingredientsArr.length; i++) {
+			//if the next ingredient is 'null' that means
+			//the rest are null, so we stop the loop
+			//and keep the actual ingredient
+			if (ingredientsArr[i] === null) {
+				break;
+			}
+			//we add each ingredient to the 'x'
+			x += `<li class="ingredient">${measurementsArr[i]+` `+ingredientsArr[i]}</li>`;
 		}
-		//we add each ingredient to the 'x'
-		x += `<li class="ingredient">${measurementsArr[i]+` `+ingredientsArr[i]}</li>`;
-    }
-    // here we populate div with id="flipcardBack" with 'x'
-    flipcardFront.innerHTML += `<ul class="ingredientList">
-    ${x}
-    </ul>
-    `;
-	flipcardBack.innerHTML+=`<p class="instructions">${data.drinks[random].strInstructions}</p>`
+		// here we populate div with id="flipcardBack" with 'x'
+		flipcardFront.innerHTML += `<ul class="ingredientList">
+		${x}
+		</ul>
+		`;
+		flipcardBack.innerHTML+=`<p class="instructions">${data.drinks[random].strInstructions}</p>`
 	});
 	// FIXME: I noticed when margarita or brandy alexander appears at random, salt and nutmeg measurement = null. 
 	// 
+}
+randomizeDrinks()
