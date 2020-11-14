22f4811c6ba984d7d574803819d282c4b244d190
Victor Tran <victortran415@gmail.com>
2020-11-13 21:49:44 -0800

    remove 2nd JS file

--- assets/script.js
+++ assets/script.js
@@ -61,38 +61,6 @@ function listCocktailsData() {
 }
 listCocktailsData()
 
-// NOTE: this will be added to Most Popular Drinks html
-var popularDrink = document.querySelector("#popular-div")
-var flipCardBack = document.querySelector(".flip-card-back")
-
-function mostPopularDrinks() {
-	fetch("https://the-cocktail-db.p.rapidapi.com/popular.php", {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-key": "1b17814413msh79012f3c019efc8p1c7fb7jsn69899d6e8800",
-			"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com"
-		}
-	})
-	.then(response =>
-		response.json())
-	.then(moreData => {
-		// console.log(moreData)
-		var drinksInfo = moreData.drinks
-		for (var i = 0 ; i < 8; i++) {
-			var drinksEle = drinksInfo[i];
-			var popDrinksName = drinksEle.strDrink
-			var popDrinksImg = drinksEle.strDrinkThumb
-			popularDrink.innerHTML += `<div class="col s3"><h6 style="text-align: center"><strong>${popDrinksName}</strong></h6><img src=${popDrinksImg} style="width: 280px; border-radius: 10px;"/><button class="save-btn"><i class="material-icons" id="heart">favorite</i></button></div>`
-		}
-		})
-	.catch(err => {
-		console.error(err);
-	})
-}
-mostPopularDrinks()
-
-
-
 
 // NOTE: Adding randomize drinks to index.html
 
@@ -211,3 +179,69 @@ fetch("https://joke3.p.rapidapi.com/v1/joke", {
 	console.error(err);
 });
 
+// This is where we put the code for the latest cocktail
+//This variable is for the div with id 'flipcardFront'
+const flipcardFrontLatest = document.getElementById("flipcardFrontLatest");
+
+//This variable is for the div with id 'flipcardBack'
+const flipcardBackLatest = document.getElementById("flipcardBackLatest");
+const latest = document.getElementById("latest");
+
+// beginning fetch request
+fetch("https://the-cocktail-db.p.rapidapi.com/latest.php", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-key": "1b17814413msh79012f3c019efc8p1c7fb7jsn69899d6e8800",
+        "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com"
+    }
+}).then(response => response.json())
+    .then(data => {
+        console.log(data)
+        for (let i = 0; i < 4; i++) {
+            let name = data.drinks[i].strDrink;
+            let imgUrl = data.drinks[i].strDrinkThumb;
+            let drinkInstructions = data.drinks[i].strInstructions;
+            let img = `<img src=${imgUrl} style="width: 200px; border-radius: 10px;"/>`;
+            let cocktailName = `<h4>${name}</h4>`;
+            let instructions = `<p>${drinkInstructions}</p>`
+            let ingredientsArray = [];
+
+            for (const [key, value] of Object.entries(data.drinks[i])) {
+                let a = ""
+                if (key.length > 13)
+                    a = key.substring(3, 13);
+                if (a === "Ingredient" && value != null)
+                    ingredientsArray.push(value);
+            }
+
+
+
+            let ingredients = '';
+            ingredientsArray.map(ingr => {
+                ingredients += `<li>${ingr}</li>`;
+            })
+
+
+            let ingredientsList = `<h5><b>Ingredients</b></h5>
+<ul>${ingredients}</ul>`;
+
+            let x = `<div class="col">
+    <div class="flip-card">
+		<div class="flip-card-inner">
+        <div id="flipcardFrontLatest" class="flip-card-front">
+        ${img}
+        ${cocktailName}
+        ${ingredientsList}
+		</div>
+        <div id="flipcardBackLatest" class="flip-card-back">
+        ${instructions}
+        <button class="save-btn"><i class="material-icons" id="heart">favorite</i></button>
+        </div>
+		</div>
+    </div>
+
+	</div>`;
+            console.log(x)
+            latest.innerHTML += x;
+        }
+    })
